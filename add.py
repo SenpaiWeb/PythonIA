d24730a2b364934b51fa8cf7463cb9fe2ba27ec4
SenpaiWeb <easycode@tuta.io>
2017-08-26 03:12:30 +0200

    0.01
    PythonIA est une IA faible qui fonctionne sur un dictionnaire modifiable qui devient alors customisé pour chaque utilisateur

---
+++ add.py
@@ -0,0 +1,21 @@
+# coding: utf8
+
+import io
+import time
+import codecs
+
+print("Système d'intégration de Q/R à l'IA")
+time.sleep(2)
+title=input("Titre: ").replace('"', "''")
+trigger=input("Quand l'IA détecte: ").replace('"', "''")
+answer=input("Que doit répondre l'IA? ").replace('"', "''")
+# options = input("Options? ") # r = regex, v = vars (r, v, rv, vr) (not working atm)
+
+with codecs.open("chat.rc", "a+", "utf-8") as chatFile:
+    chatFile.seek(0,0)
+    if (chatFile.read().find("[\""+title+"\",")==-1) and (len(trigger) > 0) and (len(answer) > 0) and (len(title) > 0):
+        # full = '"'+title+'": {"trigger": "'+trigger+'", "answer": "'+answer+'", "options": "'+options+'"}' + '\n'
+         full = '"'+title+'": {"trigger": "'+trigger+'", "answer": "'+answer+'"}\n'
+        chatFile.write(full)
+time.sleep(1)
+print("Merci à vous de participer au développement de l'IA")
---
+++ chat.py
@@ -0,0 +1,35 @@
+import io
+import time
+import random
+import re
+import codecs
+
+username = str
+name = "Jacquie" # bot name
+answers = codecs.open("chat.rc", "r", "utf-8").read() # scan file to do not have to re-open it for each operation
+def getAnswer(trigger):
+    try:
+        # catch answer with trigger
+        m = re.search('{"trigger": "'+re.escape(trigger)+'", "answer": "(.+)"}', answers)
+        return name + ": " + m.group(1)
+    except AttributeError:
+            return "Je ne comprends pas ce que vous me demandez."
+
+def getTrigger(answer):
+
+    pass
+def getAll(title):
+    insideArray = {}
+    m = re.search('"'+title+'": {"trigger": "(.+)", "answer": "(.+)"}', answers)
+    insideArray["trigger"] = m.group(1)
+    insideArray["answer"] = name + ": " + m.group(2)
+    return insideArray
+
+print(getAll("__welcome__")["answer"])
+try:
+    username = input("<Entrez votre nom> ")
+    while(True):
+        print(getAnswer(input("$ ")))
+except KeyboardInterrupt:
+            print("\nBye!")
+            exit(1)
