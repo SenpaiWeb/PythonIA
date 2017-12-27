6245e1bf1f489904aaa5bb2885ccc9eceb734ba3
SenpaiWeb <easycode@tuta.io>
2017-12-27 10:20:27 +0100

    In agreement with RFC 4627
    Removed the shit I were doing with the `custom` JSON

--- add.py
+++ add.py
@@ -1,21 +1,16 @@
 # coding: utf8
 
 import io
-import time
 import codecs
+import json
 
 print("Système d'intégration de Q/R à l'IA")
-time.sleep(1)
-title=input("Titre: ").replace('"', "''")
 trigger=input("Quand l'IA détecte: ").replace('"', "''")
 answer=input("Que doit répondre l'IA? ").replace('"', "''")
-# options = input("Options? ") # r = regex, v = vars (r, v, rv, vr) (not working atm)
 
 with codecs.open("chat.rc", "a+", "utf-8") as chatFile:
     chatFile.seek(0,0)
     if (chatFile.read().find("[\""+title+"\",")==-1) and (len(trigger) > 0) and (len(answer) > 0) and (len(title) > 0):
-        # full = '"'+title+'": {"trigger": "'+trigger+'", "answer": "'+answer+'", "options": "'+options+'"}' + '\n'
-         full = '"'+title+'": {"trigger": "'+trigger+'", "answer": "'+answer+'"}\n'
+         full = json.dumps({'trigger': trigger, 'answer': answer})
          chatFile.write(full)
-time.sleep(1)
 print("Merci à vous de participer au développement de l'IA")
