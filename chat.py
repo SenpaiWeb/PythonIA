b9a0e5e0822d5ec5ca863bed0e199282f06eac65
SenpaiWeb <easycode@tuta.io>
2017-12-27 10:32:29 +0100

    Minor update
    the IA will now save what she don't know

--- chat.py
+++ chat.py
@@ -1,32 +1,27 @@
-import io
-import time
-import random
 import re
-import codecs
+from codecs import open
+from json import dumps
 
 username = str
-name = "Jacquie" # bot name
-answers = codecs.open("chat.rc", "r", "utf-8").read() # scan file to do not have to re-open it for each operation
+name = "Edward" # bot name
+answers = open("chat.rc", "r", "utf-8").read() # scan file to do not have to re-open it for each operation
 def getAnswer(trigger):
     try:
         # catch answer with trigger
         m = re.search('{"trigger": "'+re.escape(trigger)+'", "answer": "(.+)"}', answers)
         return name + ": " + m.group(1).replace("%n", username)
     except AttributeError:
-            return "Je ne comprends pas ce que vous me demandez."
+            answer = input(name + ": Je ne comprends pas ce que vous me demandez, que dois-je répondre la prochaine fois? ")
+            addAnswer(trigger, answer)
 
 def getTrigger(answer):
     m = re.search('{"trigger": "(.+)", "answer": "'+answer+'"}', answers)
     return m.group(1)
 
-def getAll(title):
-    insideArray = {}
-    m = re.search('"'+title+'": {"trigger": "(.+)", "answer": "(.+)"}', answers)
-    insideArray["trigger"] = m.group(1)
-    insideArray["answer"] = name + ": " + m.group(2)
-    return insideArray
+def addAnswer(trigger, answer):
+    open("chat.rc", "a+", "utf-8").write(json.dumps({'trigger':trigger, 'answer':answer}))
 
-print(getAll("__welcome__")["answer"])
+print(getAnswer("__firstcontact__"))
 try:
     username = input("<Entrez votre nom> ")
     while(True):
