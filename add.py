6a5a85ddc7cb0b7e7f135a3aa1e3362e86bbb83b
SenpaiWeb <easycode@tuta.io>
2017-12-27 10:22:36 +0100

    Lightweight package

--- add.py
+++ add.py
@@ -1,16 +1,15 @@
 # coding: utf8
 
-import io
-import codecs
-import json
+from codecs import open
+from json import dumps
 
 print("Système d'intégration de Q/R à l'IA")
 trigger=input("Quand l'IA détecte: ").replace('"', "''")
 answer=input("Que doit répondre l'IA? ").replace('"', "''")
 
-with codecs.open("chat.rc", "a+", "utf-8") as chatFile:
+with open("chat.rc", "a+", "utf-8") as chatFile:
     chatFile.seek(0,0)
     if (chatFile.read().find("[\""+title+"\",")==-1) and (len(trigger) > 0) and (len(answer) > 0) and (len(title) > 0):
-         full = json.dumps({'trigger': trigger, 'answer': answer})
+         full = dumps({'trigger': trigger, 'answer': answer})
          chatFile.write(full)
 print("Merci à vous de participer au développement de l'IA")
